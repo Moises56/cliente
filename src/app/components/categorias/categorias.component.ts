fa6bf3616fd632bf8c8627e72f55c19fdd131167
Sofi <sofiafajardoperez@gmail.com>
2021-08-13 20:44:13 -0600

    componentes categorias + navcart + processcompra

--- src/app/components/categorias/categorias.component.ts
+++ src/app/components/categorias/categorias.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { CategoriasService } from '../../services/categorias.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-categorias',
@@ -6,10 +8,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categorias.component.css']
 })
 export class CategoriasComponent implements OnInit {
+  // tslint:disable-next-line:no-output-on-prefix
+  @Output() onEmpresas = new EventEmitter();
 
-  constructor() { }
+  Categorias: any = [];
+
+  constructor(
+    private categoriasService: CategoriasService,
+    private router: Router
+    ) { }
+
 
   ngOnInit(): void {
+    this.categoriasService.getCategorias()
+    .subscribe(
+      res => {
+        // console.log(res);
+        this.Categorias = res;
+      },
+      err => console.log(err)
+    );
   }
+
+  // tslint:disable-next-line:typedef
+  getCategoria(){
+    this.categoriasService.getCategorias()
+    .subscribe(
+      res => {
+        console.log(res);
+        this.Categorias = res;
+      },
+      err => console.log(err)
+    );
+  }
+
+
+  // *Enviado id A AppComponets
+  // tslint:disable-next-line:typedef
+  getEmpresas(idCat: any){
+    console.log('idCat: CompoCat: ', idCat);
+    // this.onEmpresas.emit(idCat);
+    this.categoriasService.onEmpresas.emit(idCat);
+    this.router.navigate([`/cartshop/${idCat}`]);
+    // this.router.navigateByUrl('/cartshop');
+  }
+
+
+
 
 }
